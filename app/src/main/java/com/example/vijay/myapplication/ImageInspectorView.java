82dd58d439dad3c6a6703c895dc461eb74ed9cca
Admin <admin@Admins-Mac-mini.local>
2018-04-21 19:34:13 +0530

    added toolbar button actions

--- app/src/main/java/com/example/vijay/myapplication/ImageInspectorView.java
+++ app/src/main/java/com/example/vijay/myapplication/ImageInspectorView.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import java.util.Random;
 
@@ -19,6 +21,43 @@ public class ImageInspectorView extends Fragment {
         Random rnd = new Random();
         view.setBackgroundColor(Color.rgb(rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255)));
 
+        ImageButton addButton = (ImageButton)view.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addButtonAction();
+            }
+        });
+
+        final ImageButton cameraButton = (ImageButton)view.findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraButtonAction();
+            }
+        });
+
+        ImageButton detailsButton = (ImageButton)view.findViewById(R.id.detailsButton);
+        detailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detailsButtonAction();
+            }
+        });
+
         return view;
     }
+
+    void addButtonAction() {
+        Log.d("ImageInspectorView", "addButtonAction");
+    }
+
+    void cameraButtonAction() {
+        Log.d("ImageInspectorView", "cameraButtonAction");
+    }
+
+    void detailsButtonAction() {
+        Log.d("ImageInspectorView", "detailsButtonAction");
+    }
+
 }
