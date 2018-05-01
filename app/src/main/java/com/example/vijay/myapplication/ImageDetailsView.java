c6d3ec39a74db8ac8a1d9878f79a337c27e5aef5
vijay <alterechoxii@gmail.com>
2018-05-01 15:27:20 +0530

    added and linked layout for ImageDetailsView

--- app/src/main/java/com/example/vijay/myapplication/ImageDetailsView.java
+++ app/src/main/java/com/example/vijay/myapplication/ImageDetailsView.java
@@ -3,9 +3,12 @@ package com.example.vijay.myapplication;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.View;
+import android.widget.RelativeLayout;
 
-public class ImageDetailsView extends View {
+public class ImageDetailsView extends RelativeLayout {
 
     ImageDetailsView(Context ctx) {
         super(ctx);
@@ -14,11 +17,19 @@ public class ImageDetailsView extends View {
 
     ImageDetailsView(Context ctx, AttributeSet attr) {
         super(ctx, attr);
+
+        LayoutInflater inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.view_image_details, null, false);
+
         init();
     }
 
 
     private void init() {
         this.setBackgroundColor(Color.BLUE);
+        LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.view_image_details, this, true);
+        //or
+        //inflate(getContext(), R.layout.view_image_details, this);
     }
 }
