ca40d41a7589d68e96fffcb274d5af459ff67ec2
vijay <alterechoxii@gmail.com>
2018-05-03 16:39:16 +0530

    remove action bar

--- app/src/main/java/com/vjc/imagecompare/CompareActivity.java
+++ app/src/main/java/com/vjc/imagecompare/CompareActivity.java
@@ -3,11 +3,19 @@ package com.vjc.imagecompare;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CompareActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //* hides the status bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        this.getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+
+        getSupportActionBar().hide();
 
         setContentView(R.layout.activity_compare);
     }
